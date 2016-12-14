840237a227b45b519e4381ccfcec781bd0f233a4
Manon Peberat <manon.peberat@sfr.fr>
2016-12-14 12:46:55 +0100

    changement 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :classement_series
-  root 'classement_series#index'
+  root to: 'classement_series#index'
 end
